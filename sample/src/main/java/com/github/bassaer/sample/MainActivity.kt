73e5449f1cea0b1ee29401b1646d8cc1e03263ea
Tsubasa Nakayama <app.nakayama@gmail.com>
2018-04-30 18:47:49 +0900

    add basic func

--- sample/src/main/java/com/github/bassaer/sample/MainActivity.kt
+++ sample/src/main/java/com/github/bassaer/sample/MainActivity.kt
@@ -2,8 +2,6 @@ package com.github.bassaer.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.FrameLayout
-import com.github.bassaer.tickerview.TickerTextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val layout = findViewById<FrameLayout>(R.id.frame_layout)
-        val tickerView = TickerTextView(this)
-        layout.addView(tickerView)
+
 
     }
 }
--- tickerview/src/main/java/com/github/bassaer/tickerview/TickerTextView.kt
+++ tickerview/src/main/java/com/github/bassaer/tickerview/TickerTextView.kt
@@ -1,22 +1,26 @@
 package com.github.bassaer.tickerview
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.PixelFormat
 import android.graphics.PorterDuff
+import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import java.util.concurrent.ScheduledExecutorService
+
+
 
 
 /**
  * Ticker Text View
  * Created by nakayama on 2018/04/30.
  */
-class TickerTextView(context: Context) : SurfaceView(context), SurfaceHolder.Callback {
+class TickerTextView(context: Context, attrs: AttributeSet) : SurfaceView(context, attrs), SurfaceHolder.Callback {
 
     private var surfaceHolder = holder
-    private lateinit var scheduledExecutorService: ScheduledExecutorService
     private val speed = 1
     private var viewWidth = 0
     private var viewHeight = 0
@@ -24,22 +28,16 @@ class TickerTextView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
     private val text = "Hello World"
     private var textWidth = 0f
     private var wholeTextWidth = 0f
-    private var textSize = context.resources.getDimensionPixelSize(R.dimen.text_size)
+    private var textSize = 0
     private var textHeight = 0f
     private var currentPosition = 0f
     private var scrollTime = 10
+    private var density = 1f
 
-    init {
-        surfaceHolder = holder
-        surfaceHolder.addCallback(this)
-        paint = Paint()
-    }
-
-    fun draw(X: Float, Y: Float) {
+    fun draw(x: Float, y: Float) {
         val canvas = surfaceHolder.lockCanvas()
         canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
-        canvas.drawText(text, X, Y, paint)
-
+        canvas.drawText(text, x, y, paint)
         surfaceHolder.unlockCanvasAndPost(canvas)
     }
 
@@ -51,8 +49,7 @@ class TickerTextView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
     }
 
     override fun surfaceCreated(holder: SurfaceHolder?) {
-
-
+        draw(currentPosition, textHeight)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -61,7 +58,7 @@ class TickerTextView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
         viewHeight = MeasureSpec.getSize(heightMeasureSpec)
         textWidth = paint.measureText(text)
         wholeTextWidth = viewWidth + textWidth
-        textHeight = (viewHeight + getFontHeight(textSize.toFloat())) / 2 + paddingTop - paddingBottom + 2
+        textHeight = (viewHeight + getFontHeight(textSize.toFloat()/ density)) / 2 + paddingTop - paddingBottom + 2
 
     }
 
@@ -72,20 +69,20 @@ class TickerTextView(context: Context) : SurfaceView(context), SurfaceHolder.Cal
         return Math.ceil(metrics.descent.toDouble() - metrics.ascent.toDouble()).toFloat()
     }
 
-    internal inner class TickerThread : Runnable {
-        override fun run() {
-            while (true) {
-                draw(viewWidth - currentPosition, textHeight)
-                currentPosition += speed
-                if (currentPosition > wholeTextWidth) {
-                    currentPosition = 0f
-                    --scrollTime
-                }
-                if (scrollTime <= 0) {
-                   break
-                }
-            }
-        }
+
+    init {
+        surfaceHolder = holder
+        surfaceHolder.addCallback(this)
+        paint = Paint()
+        paint.color = Color.BLACK
+        textSize = context.resources.getDimensionPixelSize(R.dimen.text_size)
+        paint.textSize = textSize.toFloat()
+        setZOrderOnTop(true)
+        holder.setFormat(PixelFormat.TRANSLUCENT)
+        val metric = DisplayMetrics()
+        (context as Activity).windowManager.defaultDisplay.getMetrics(metric)
+        density = metric.density
+        isFocusable = true
     }
 
 }
